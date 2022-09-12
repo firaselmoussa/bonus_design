147bd974699184a8645ccbc2696414a0478dc26e
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-12 20:45:35 +0300

    implemented email validation

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ menu_btn.addEventListener('click', ()=>{
 
 // CONTACT FORM INPUTS VALIDATION
 let send_btn = document.getElementById('send-btn');
-let error_container = document.getElementById('error-container');
-let error_popup = document.createElement('p');
-error_popup.classList.add('contact-error')
+let error_popup= document.getElementById('error-popup')
 let name_input = document.getElementById('name-input');
 let email_input = document.getElementById('email-input');
 
@@ -27,20 +25,32 @@ send_btn.addEventListener('click', ()=>{
 
     // Name validation
     if(name_input.value.length < 5){
-        error_popup.innerText = 'name too short(minimum 5characters)';
-        error_container.append(error_popup);
-    };
+        error_popup.innerText = 'name too short(minimum 5 characters)';
+        error_popup.style.display = 'flex';
+    }else{
+        error_popup.style.display = 'none';
+    }
 
     // Email validation
-    var email_parts = email_input.value.split('@', 2);
 
-    if(email_parts[0].length < 3){
-        error_popup.innerText = 'gmail name is too short';
-        error_container.append(error_popup);
-    }else if(email_parts[1].length <5){
-        error_popup.innerText = 'gmail extension is too short';
-        error_container.append(error_popup);
-    };
-    
+    let email_list = email_input.value.split('');
+
+    let email_parts = email_input.value.split('@', 2);
+    let is_email = false;
+
+    email_list.forEach(element => {
+        if(element == '@'){
+            is_email = true;
+            console.log('valid', element);
+            
+        }
+    });
+
+    if(!is_email){
+        error_popup.innerText = 'email invalid (include "@")';
+        error_popup.style.display = 'flex';
+    }
+
+
 
 });

4dbe720f3f1441aee78b99d8cc5e81c5c0f89c59
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-09-14 09:34:17 +0300

    implemented form submission

---
+++ backend/add_message.php
@@ -0,0 +1,30 @@
+<?php
+include("connection.php");
+
+if(isset($_POST["full_name"]) && isset($_POST["email"]) && isset($_POST["phone_number"]) && isset($_POST["message"])){
+    if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+
+        
+$full_name = $_POST["full_name"];
+$email = $_POST["email"];
+$phone_number = $_POST["phone_number"];
+$message = $_POST["messge"];
+
+
+$query = "INSERT INTO articles(full_name, email, phone_number, message) VALUE (" .$full_name. "," .$email. "," .$phone_number. "," .$message. "?)";
+
+$query = $mysqli->prepare("INSERT INTO message_info(full_name, email) VALUE (?, ?)");
+$query->bind_param("ss", $full_name, $email, $phone_number, $message);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+
+
+    };
+};
+
+?>
